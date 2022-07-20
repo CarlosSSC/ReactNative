aae62511fc0c80ea5649e696704dae27ec2887a1
CarlosSSC <a6520150015@utch.edu.mx>
2022-07-19 19:31:30 -0600

    Home page

--- App.js
+++ App.js
@@ -1,20 +1,69 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, SafeAreaView, Button, TouchableOpacity} from 'react-native';
 
 export default function App() {
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+
+      <View style={{ flex: 3, backgroundColor: 'rgb(248, 113, 113)', alignItems: 'center', justifyContent: 'center'}}>
+        <Text style={styles.whiteText}>Metal Heroes</Text>
+        <Image style={styles.photo} source={require('./assets/manwithguitar.png')}/>
+        <Text style={styles.whiteSmallText}>The best way to learn how to play an instrument!</Text>
+      </View>
+
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+        <View>
+          <Button
+          color={'rgb(34, 211, 238)'}
+          title='Create an Account' onPress={() => console.log('button tapped')}/>
+        </View>
+
+        <View style={styles.space}/> 
+
+        <View >
+          <Button 
+          color={'gray'}
+          title  ='or Login' onPress={() => console.log('2nd button tapped')}
+          />
+        </View>
+
+
+      </View>
+
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flexDirection: 'column',
   },
+  whiteText:{
+    fontWeight: 'bold',
+    color: 'white',
+    fontSize: 45,
+  },
+  whiteSmallText:{
+    fontWeight: 'bold',
+    color: 'white',
+    fontSize: 35
+  },
+  photo: {
+    width: 300,
+    height: 260,
+  },
+  button:{
+    borderRadius: 16,
+    borderColor: 'rgb(34, 211, 238)',
+    padding: 5,
+    backgroundColor : 'rgb(34, 211, 238)'
+  },
+  space: {
+    width: 20,
+    height: 20,
+  },
+
 });
